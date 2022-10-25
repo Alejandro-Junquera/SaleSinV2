97fabfa47480ee07ad07de27aa807b26c70d54ca
Alejandro Junquera <junquera.maale21@cadiz.salesianos.edu>
2022-10-25 08:42:52 +0200

    .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,7 +16,7 @@ class AdminController extends Controller
     {
         $users = User::latest()->paginate(5);
   
-        return view('users.index',compact('users'))
+        return view('admin.index',compact('users'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
@@ -38,15 +38,7 @@ class AdminController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-        ]);
-  
-        User::create($request->all());
-   
-        return redirect()->route('users.index')
-                        ->with('success','Product created successfully.');
+       //
     }
 
     /**
