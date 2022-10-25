127305dc73f1d369f932f2123062b124f200d735
MartaRondan96 <martarondan96@gmail.com>
2022-10-25 08:11:49 +0200

    modelos

--- app/articles.php
+++ app/articles.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class articles extends Model
 {
-    protected $table='articles';
+    //protected $table='articles';
 
    // protected $fillable=['id','title','image','deleted','description','cicle_id'];
 }
