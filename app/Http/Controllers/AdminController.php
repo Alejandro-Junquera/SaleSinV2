3b8402bcdffa0af968b53c672899b1f17cb31406
Alejandro Junquera <junquera.maale21@cadiz.salesianos.edu>
2022-10-25 08:36:33 +0200

    admincontroller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class AdminController extends Controller
 {
     /**
      * Display a listing of the resource.
