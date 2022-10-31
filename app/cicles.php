c3624281918961b9e10626428fdfa10faad9cea0
MartaRondan96 <martarondan96@gmail.com>
2022-10-31 13:08:33 +0100

    Mayus

--- app/cicles.php
+++ app/cicles.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Cicles extends Model
 {
     protected $table='cicles';
+    //.
     //protected $fillable=['id','name','img','deleted'];
 }
--- app/offers.php
+++ app/offers.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offers extends Model
 {
+    //.
     //protected $table='offers';
     //protected $fillable=['id','title','description','date_max','num_candidates','deletes'];
 }
