f4e2008ee3a7c9a87ad2cc7fe0ccc3f34d4ce373
Alejandro Junquera <junquera.maale21@cadiz.salesianos.edu>
2022-12-14 08:44:57 +0100

    arreglo largo tabla

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,11 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />   
+    <style>
+        .table td{
+            max-width:500px;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
